d5228f416e1921936f7988b0adff773504afdd7a
= <=>
2024-03-21 14:34:25 +0100

    feat: create calculator model

---
+++ barberini_calculator/Models/Calculator.swift
@@ -0,0 +1,43 @@
+//
+//  Calculator.swift
+//  barberini_calculator
+//
+//  Created by noah on 21.03.2024.
+//
+
+struct Calculator {
+    
+    
+    var displayText: String {
+        return "0"
+    }
+    
+    mutating func setDigit(_ digit: Digit) {
+        
+    }
+    
+    mutating func setOperation(_ operation: ArithmeticOperation) {
+        
+    }
+    
+    mutating func toggleSign() {
+        
+    }
+    
+    mutating func setPercent() {
+        
+    }
+    mutating func setDecimal() {
+        
+    }
+    
+    mutating func evaluate() {
+    }
+    
+    mutating func allClear() {
+    }
+    
+    mutating func clear() {
+        
+    }
+}
