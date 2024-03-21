c1b0445273f4eaa20a6b6ba7ef09617439ac680c
= <=>
2024-03-21 13:37:32 +0100

    Initial Commit

---
+++ barberini_calculator/barberini_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  barberini_calculatorApp.swift
+//  barberini_calculator
+//
+//  Created by hyil on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct barberini_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
